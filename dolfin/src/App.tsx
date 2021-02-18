3573c03966e444e4a13184d0266bb329fb51377b
tejasag <tejasagarwalbly@gmail.com>
2021-02-18 21:23:33 +0530

    added navbar to dolfin

---
+++ dolfin/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/App.css';
+import {Home} from "./pages/home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;

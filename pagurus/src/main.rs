7158ece57db915b179ed4aad216a279b96955711
tejasag <tejasagarwalbly@gmail.com>
2021-02-25 13:13:35 +0530

    fixed pagurus

--- pagurus/src/main.rs
+++ pagurus/src/main.rs
@@ -7,7 +7,6 @@ extern crate rocket_contrib;
 extern crate diesel;
 
 mod schema;
-mod utils;
 
 use diesel::prelude::*;
 use rocket::request::Request;
@@ -15,14 +14,12 @@ use rocket_contrib::{databases::diesel::PgConnection, json::Json};
 use serde::{Deserialize, Serialize};
 
 #[post("/create-user", format = "json", data = "<input>")]
-fn create(input: Json<InsertableUser>, db_conn: PostgresDB) -> Json<User> {
+fn create(input: Json<RequestUserBody>, db_conn: PostgresDB) -> Json<User> {
     Json(InsertableUser::new(
         input.username,
         input.display_name,
         input.email,
         input.password,
-        input.created_at,
-        input.last_updated,
         &db_conn.0,
     ))
 }
@@ -33,11 +30,11 @@ fn index(_db_conn: PostgresDB) -> &'static str {
 }
 
 #[catch(503)]
-fn service_not_available(_req: &Request) -> &'static str {
+fn unavailable(_req: &Request) -> &'static str {
     "Service is not available"
 }
 
-#[derive(Identifiable, Queryable, Associations, PartialEq, Debug, Serialize, Deserialize)]
+#[derive(Queryable, Associations, PartialEq, Debug, Serialize, Deserialize)]
 #[table_name = "users"]
 pub struct User {
     pub username: String,
@@ -45,11 +42,11 @@ pub struct User {
     pub email: String,
     // remember to hash
     pub password: String,
-    pub created_at: u64,
-    pub last_updated: Option<u64>,
+    pub created_at: chrono::NaiveDateTime,
+    pub last_updated: Option<chrono::NaiveDateTime>,
 }
 
-use rocket::response::content::JavaScript;
+use chrono::NaiveDateTime;
 use schema::users;
 
 #[derive(Insertable, Serialize, Deserialize)]
@@ -59,8 +56,16 @@ pub struct InsertableUser<'a> {
     pub display_name: &'a str,
     pub email: &'a str,
     pub password: &'a str,
-    pub created_at: &'a u64,
-    pub last_updated: Option<&'a u64>,
+    pub created_at: chrono::NaiveDateTime,
+    pub last_updated: Option<chrono::NaiveDateTime>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RequestUserBody<'a> {
+    pub username: &'a str,
+    pub display_name: &'a str,
+    pub email: &'a str,
+    pub password: &'a str,
 }
 
 impl<'a> InsertableUser<'a> {
@@ -69,19 +74,26 @@ impl<'a> InsertableUser<'a> {
         display_name: &'a str,
         email: &'a str,
         password: &'a str,
-        created_at: u64,
-        last_updated: Option<u64>,
+        //   created_at: chrono::NaiveDateTime,
+        //  last_updated: Option<chrono::NaiveDateTime>,
         conn: &PgConnection,
     ) -> User {
-        use schema::users;
+        use chrono::{DateTime, Utc};
+        use std::time::{SystemTime, UNIX_EPOCH};
+
+        let start = SystemTime::now();
+        let since_the_epoch = start
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards");
+        let rightnow = NaiveDateTime::from_timestamp(since_the_epoch.as_secs() as i64, 0);
 
         let user = Self {
             username,
             display_name,
             email,
             password,
-            created_at,
-            last_updated,
+            created_at: rightnow,
+            last_updated: Option::from(rightnow),
         };
 
         diesel::insert_into(users::table)
@@ -97,7 +109,7 @@ struct PostgresDB(PgConnection);
 fn main() {
     rocket::ignite()
         .attach(PostgresDB::fairing())
-        .register(catchers![serivce_not_available])
+        .register(catchers![unavailable])
         .mount("/", routes![index, create])
         .launch();
 }
@@ -113,10 +125,38 @@ mod test {
     fn test_index() {
         let rocket = rocket::ignite()
             .attach(PostgresDB::fairing())
-            .register(catchers![service_not_available])
+            .register(catchers![unavailable])
             .mount("/", routes![index, create]);
         let client = Client::new(rocket).expect("invalid rocket instance");
         let mut response = client.get("/").dispatch();
-        assert_eq!(response.body_string(), Some("Welcome to pagurus!").into())
+        assert_eq!(response.body_string(), Some("Welcome to pagurus!".into()))
+    }
+
+    #[test]
+    fn test_user_create() {
+        let rocket = rocket::ignite()
+            .attach(PostgresDB::fairing())
+            .register(catchers![unavailable])
+            .mount("/", routes![index, create]);
+        let client = Client::new(rocket).expect("invalid rocket instance");
+        let mut response = client
+            .post("/create-user")
+            .header(ContentType::JSON)
+            .body(
+                r##"{
+                "username": "tejasagarwal",
+                "display_name": "Tejas Agarwal",
+                "email": "tejas@agarw.al",
+                "password": "secretPassword"
+            }"##,
+            )
+            .dispatch();
+        let response_body = response.body_string().expect("Response body");
+        let user: User =
+            serde_json::from_str(&response_body.as_str()).expect("Valid user response.");
+        assert_eq!(user.username, String::from("tejasagarwal"));
+        assert_eq!(user.display_name, String::from("Tejas Agarwal"));
+        assert_eq!(user.email, String::from("tejas@agarw.al"));
+        assert_eq!(user.password, String::from("secretPassword"));
     }
 }
